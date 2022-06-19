6408b0e17adbedaa6a5cc54684972e5e347af85b
diegoaichele <37969116+diegoaichele@users.noreply.github.com>
2022-06-18 21:50:06 -0400

    feat: :closed_lock_with_key:  Add Table Card, with information about a unique card.

--- create_database.py
+++ create_database.py
@@ -3,3 +3,14 @@ import os, sqlite3
 os.mkdir("images")
 
 con = sqlite3.connect('cipher_fireemblem.db')
+
+cur.execute("""CREATE TABLE skill (
+                ID varchar(15)NOT NULL,
+                skill varchar(255)
+            );""")
+
+cur.execute("""CREATE TABLE affinities (
+                ID varchar(15)NOT NULL,
+                affinities varchar(255)
+
+            );""")
