7edc11a98eb9f8e1c499c27f9afa52a071806db3
diegoaichele <37969116+diegoaichele@users.noreply.github.com>
2022-06-18 21:52:42 -0400

    feat: :closed_lock_with_key:  Add cursor and Card information

--- create_database.py
+++ create_database.py
@@ -4,6 +4,27 @@ os.mkdir("images")
 
 con = sqlite3.connect('cipher_fireemblem.db')
 
+cur = con.cursor()
+
+cur.execute("""CREATE TABLE card (
+                ID varchar(20)NOT NULL,
+                name varchar(55) NOT NULL,
+                imageurl varchar(255),
+                class varchar(255),
+                cost varchar(255),
+                symbol1 varchar(100),
+                symbol2 varchar(100),
+                attack int,
+                suppport int,
+                range int,
+                quote varchar(500),
+                illustrator varchar(60),
+                comment varchar(500),
+
+                PRIMARY KEY (ID)
+            );""")
+
+
 cur.execute("""CREATE TABLE skill (
                 ID varchar(15)NOT NULL,
                 skill varchar(255)
